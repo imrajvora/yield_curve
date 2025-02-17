682ba23b523811670cfb49d3d652aa65cd20d443
imrajvora <imrajvora@gmail.com>
2025-02-17 18:33:03 +0530

    3rd commit

--- yield_curve.py
+++ yield_curve.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
+import yfinance as yf
 import matplotlib.pyplot as plt
 from click.formatting import iter_rows
+from numpy.core.defchararray import index
 from setuptools.config.expand import canonic_data_files
 from sklearn.conftest import pyplot
 
 tenor = [2, 5, 10, 30]
-yields = [3, 5.75, 7.5, 8]
+yields = [3, 4.5, 5.5, 7.5]
 
 df = pd.DataFrame([tenor, yields]).transpose()
 df = df.set_index(0)
@@ -28,7 +30,7 @@ def intrpolte (data, lower, upper, mid: float):
     return df.sort_values(1)
 
 def yieldcurve():
-    for i in range(30):
+    for i in np.arange(0,30.5,0.5):
         if 2<i<5:
             intrpolte(df,2,5,i)
         if 5<i<10:
@@ -57,6 +59,17 @@ def fwdcurve(x):
             xYYz.append(f"{row[0]}YX{row[0]+x}Y")
     df2 = pd.DataFrame(np.transpose(crve), columns= [f"{x}YR forward curve %"], index = xYYz)
 
+    xY = []
+    yx = []
+    for row in df2.iterrows():
+        if float(row[0][0:2])<10:
+            xY.append((row[0][0:3]))
+            yx.append(row[0][5:-1])
+        else:
+            xY.append((row[0][0:4]))
+            yx.append(row[0][6:-1])
+    df2[f"{x}Y rate starting @"] = xY
+    df2[f"{x}Y rate ending @"] = yx
     return df2
 
 def discount_curve():
@@ -114,15 +127,15 @@ def bond_npv1(n, cf, fv, curve_drift, key_rate, rate_delta):
 
 def curveshiftanalysis(n, cf, fv, curve_drift, key_rate, rate_delta):
     print("***CURVE SHIFT ANALYSIS BEGINS***")
+    print("  ")
     print(f"BOND INFO: {n}Y bond with {cf}$ cashflow paid annually with a face value of {fv}")
     print((bond_npv(n, cf, fv)))
     print(f"Applying curve drift of {curve_drift}bps across the curve with special {rate_delta}bps sensitivity change to "
           f"key rate tenor of {key_rate}Y")
     print((bond_npv1(n, cf, fv, curve_drift, key_rate, rate_delta)))
+    print("  ")
     print("***CURVE SHIFT ANALYSIS ENDS***")
     print("  ")
-
-curveshiftanalysis(10, 5, 100, 3, 10, 2)
 
 def curve_trade_be(trade_type: str, leg_1, leg_2, horizon):
     yieldcurve()
@@ -132,12 +145,15 @@ def curve_trade_be(trade_type: str, leg_1, leg_2, horizon):
         trade = "steepen"
     else:
         trade = "flatten"
+
     print("***TRADE INFO BEGINS***")
+    print("  ")
     if trade_type == "steepner":
         og_trade_spread = yieldcurve().loc[leg_1] - yieldcurve().loc[leg_2]
         fwd_be = fwdcurve(horizon).iloc[leg_1 - horizon - 2] - fwdcurve(horizon).iloc[leg_2 - horizon - 2]
         fwd_be = (round(fwd_be.values[0], 3))
-        print(f'*Putting on a curve steepner by going long {leg_1}s and short {leg_2}s at {round(og_trade_spread.values[0],4)} bps')
+        print(f'*Putting on a curve steepner by going long {leg_1}s ({yieldcurve().loc[leg_1].values[0]}%)'
+              f' and short {leg_2}s ({yieldcurve().loc[leg_2].values[0]}%) at {round(og_trade_spread.values[0],4)} bps')
         print(f'*After {horizon}Y, the orginal trade would have become a long {leg_1 - horizon}s and short {leg_2-horizon}s steepner')
         print(f'*The break-even suggested by the {horizon}Y forward curve for long {leg_1 - horizon}s and short {leg_2 - horizon}s is {fwd_be}bps')
         print(f'*Enter the trade only if you think the spread will {trade} by {fwd_be-round(og_trade_spread.values[0],0)}bps')
@@ -146,15 +162,58 @@ def curve_trade_be(trade_type: str, leg_1, leg_2, horizon):
         fwd_be = fwdcurve(horizon).iloc[leg_2 - horizon - 2] - fwdcurve(horizon).iloc[leg_1 - horizon - 2]
         fwd_be = (round(fwd_be.values[0], 4))
         print(fwd_be)
-        print(f'*Putting on a curve flattner by going short {leg_1}s and long {leg_2}s at {round(og_trade_spread.values[0],4)} bps')
+        print(f'*Putting on a curve flattner by going short {leg_1}s ({yieldcurve().loc[leg_1].values[0]}%)'
+              f' and long {leg_2}s ({yieldcurve().loc[leg_2].values[0]}%) at {round(og_trade_spread.values[0],4)} bps')
         print(f'*After {horizon}Y, the orginal trade would have become a short {leg_1 - horizon}s and long {leg_2 - horizon}s flattner'
               f' {round(og_trade_spread.values[0],4)} bps')
         print(f'*The break-even suggested by the {horizon}Y forward curve for short {leg_2 - horizon}s and long {leg_1 - horizon}s is {fwd_be}bps')
         print(f'*Enter the trade only if you think the spread will {trade} by {fwd_be-round(og_trade_spread.values[0],0)}bps')
+    print("  ")
     print("***TRADE INFO ENDS***")
+    print("  ")
 
-curve_trade_be("steepner", 5,7,3)
 
-print(yieldcurve())
-print(discount_curve())
-print(fwdcurve(1))
+def getlast_close():
+    ticker = input("What ticker do you want to find the closing price for? ")
+    print(f"{yf.Ticker(str(ticker)).info['previousClose']}$")
+
+def get_yieldcurves():
+    x = float(input("Please enter the forward rate curve you would like to see --->  "))
+    print(yieldcurve().sort_values(by=0))
+    print(discount_curve().sort_index())
+    print(fwdcurve(x))
+    plt.subplot(3,1,1)
+    plt.plot(yieldcurve().sort_values(by=0))
+    plt.axis([0,31,min(yields)-1,max(yields)+1])
+    plt.xlabel("Tenors")
+    plt.ylabel("Yields")
+    plt.title("Yield Curve")
+    # plt.show()
+    plt.subplot(3, 1, 2)
+    plt.plot(discount_curve().sort_index()['discount factor'])
+    plt.xlabel("Tenors")
+    plt.ylabel("Discount Factor")
+    plt.title("Discount Curve")
+    plt.axis([0, 31, min(discount_curve()['discount factor']-0.1), max(discount_curve()['discount factor']+0.1)])
+    # plt.show()
+    plt.subplot(3, 1, 3)
+    plt.plot(fwdcurve(x)[f'{x}Y rate starting @'], round(fwdcurve(x)[f'{x}YR forward curve %'],5))
+    plt.xlabel("Tenors")
+    plt.ylabel("Yields")
+    plt.title(f"{x}Y Forward Curve")
+    plt.show()
+
+
+# curve_trade_be("steepner", 5,7,3)
+# curveshiftanalysis(10, 5, 100, 2, 4, -1)
+
+get_yieldcurves()
+
+
+# for row in fwdcurve(3).iterrows():
+#     print(row[0][0:4])
+# print(yieldcurve())
+# print(discount_curve())
+
+
+
